ad90616fc5a4c4659dd5ec91207bd124b9ae505e
Gerardp <agerardo1397@gmail.com>
2020-09-21 15:28:35 -0500

    minor changes to login

--- app.js
+++ app.js
@@ -5,74 +5,118 @@ const modalFormBox = document.querySelector(".modal-box");
 const modalForm = document.querySelector(".modal-form");
 
 modal.addEventListener("click", function (e) {
-    if (e.target.className !== "modal-box") {
-        // closeModal();
-    }
-})
+  if (e.target.className !== "modal-box") {
+    // closeModal();
+  }
+});
 crossModal.addEventListener("click", function () {
-    closeModal();
-})
+  closeModal();
+});
 
 for (let i = 0; i < menuForms.length; i++) {
-    menuForms[i].addEventListener("click", function () {
-        openModal();
-        let div = document.createElement("div");
-        if (menuForms[i].dataset.target === "register") {
+  menuForms[i].addEventListener("click", function () {
+    openModal();
+    let div = document.createElement("div");
+    if (menuForms[i].dataset.target === "register") {
+      let divTitle = document.createElement("div");
+      divTitle.appendChild(document.createTextNode("Register"));
+      divTitle.classList.add("mf-title");
 
-            let divTitle = document.createElement("div");
-            divTitle.appendChild(document.createTextNode("Register"));
-            divTitle.classList.add("mf-title");
+      let divSubTitle = document.createElement("div");
+      divSubTitle.appendChild(
+        document.createTextNode("Create your account now")
+      );
+      divSubTitle.classList.add("mf-subTitle");
 
-            let divSubTitle = document.createElement("div");
-            divSubTitle.appendChild(document.createTextNode("Create your account now"));
-            divSubTitle.classList.add("mf-subTitle");
+      let userInput = document.createElement("input");
+      userInput.classList.add("mf-input");
+      userInput.placeholder = "User Name *";
+      let emailInput = document.createElement("input");
+      emailInput.classList.add("mf-input");
+      emailInput.placeholder = "Email *";
 
-            let userInput = document.createElement("input");
-            userInput.classList.add("mf-input");
-            userInput.placeholder = "User Name *"
-            let emailInput = document.createElement("input");
-            emailInput.classList.add("mf-input");
-            emailInput.placeholder = "Email *"
+      let passInput = document.createElement("input");
+      passInput.classList.add("mf-input");
+      passInput.placeholder = "Password *";
 
-            let passInput = document.createElement("input");
-            passInput.classList.add("mf-input");
-            passInput.placeholder = "Password *"
+      let confirmInput = document.createElement("input");
+      confirmInput.classList.add("mf-input");
+      confirmInput.placeholder = "Repeat Password *";
 
-            let confirmInput = document.createElement("input");
-            confirmInput.classList.add("mf-input");
-            confirmInput.placeholder = "Repeat Password *"
+      let confirmBtn = document.createElement("div");
+      confirmBtn.appendChild(document.createTextNode("Register"));
+      confirmBtn.classList.add("mf-btn");
 
+      div.appendChild(divTitle);
+      div.appendChild(divSubTitle);
+      div.appendChild(userInput);
+      div.appendChild(emailInput);
+      div.appendChild(passInput);
+      div.appendChild(confirmInput);
+      div.appendChild(confirmBtn);
 
-            let confirmBtn = document.createElement("div");
-            confirmBtn.appendChild(document.createTextNode("Register"));
-            confirmBtn.classList.add("mf-btn");
+      modalForm.appendChild(div);
+    } else {
+      let divTitle = document.createElement("div");
+      divTitle.appendChild(document.createTextNode("Login"));
+      divTitle.classList.add("mf-title");
 
+      let userInput = document.createElement("input");
+      userInput.classList.add("mf-input");
+      userInput.placeholder = "User Name *";
 
-            div.appendChild(divTitle);
-            div.appendChild(divSubTitle);
-            div.appendChild(userInput);
-            div.appendChild(emailInput);
-            div.appendChild(passInput);
-            div.appendChild(confirmInput);
-            div.appendChild(confirmBtn);
+      let passInput = document.createElement("input");
+      passInput.classList.add("mf-input");
+      passInput.placeholder = "Password *";
 
-            modalForm.appendChild(div);
-        } else {
+      let confirmBtn = document.createElement("div");
+      confirmBtn.appendChild(document.createTextNode("Login"));
+      confirmBtn.classList.add("mf-btn");
 
-        }
-    })
+      let divCheckBox = document.createElement("div");
+      let checkBox = document.createElement("input");
+      checkBox.type = "checkbox";
+      checkBox.id = "rememberMe";
+
+      let label = document.createElement("label");
+      label.appendChild(document.createTextNode("Remember me"));
+      label.classList.add("label-login");
+      divCheckBox.appendChild(checkBox);
+      divCheckBox.appendChild(label);
+
+      let forgotPass = document.createElement("div");
+      forgotPass.appendChild(document.createTextNode("forgot password"));
+      let arrowFP = document.createElement("div");
+      arrowFP.classList.add("arrowFP");
+      forgotPass.appendChild(arrowFP);
+      forgotPass.classList.add("forgot-pass");
+
+      let divUserManage = document.createElement("div");
+      divUserManage.classList.add("row");
+
+      divUserManage.appendChild(divCheckBox);
+      divUserManage.appendChild(forgotPass);
+
+      div.appendChild(divTitle);
+      div.appendChild(userInput);
+      div.appendChild(passInput);
+      div.appendChild(divUserManage);
+      div.appendChild(confirmBtn);
+
+      modalForm.appendChild(div);
+    }
+  });
 }
 
 function closeModal() {
-    modal.style.opacity = 0;
-    modal.style.pointerEvents = "none";
-    while (modalForm.firstChild) {
-        modalForm.removeChild(modalForm.firstChild)
-    }
-
+  modal.style.opacity = 0;
+  modal.style.pointerEvents = "none";
+  while (modalForm.firstChild) {
+    modalForm.removeChild(modalForm.firstChild);
+  }
 }
 
 function openModal() {
-    modal.style.opacity = 1;
-    modal.style.pointerEvents = "auto";
+  modal.style.opacity = 1;
+  modal.style.pointerEvents = "auto";
 }
